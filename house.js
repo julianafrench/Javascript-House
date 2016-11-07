328d6373ca9aaec8cbce53bf91b83c2a9263bdb5
julianafrench <julianaefrench@gmail.com>
2016-11-07 13:36:11 -0800

    rough outline of house

--- house.js
+++ house.js
@@ -9,15 +9,7 @@ function drawGuides() {
     var xGd2 = 10;
     var xGd3 = 290;
     var yGd3 = 180;
-    
-    // initial height of house
-    ctx.beginPath();
-    ctx.moveTo(xGd1, yGd1);
-    ctx.lineTo(xGd1, yGd2);
-    ctx.strokeStyle = "black";
-    ctx.stroke();
-    ctx.closePath();
-    
+
     // perspective line
     ctx.beginPath();
     ctx.moveTo(0, yGd3);
@@ -26,42 +18,64 @@ function drawGuides() {
     ctx.stroke();
     ctx.closePath();
     
+    // left roof panel
     ctx.beginPath();
-    ctx.moveTo(xGd3, yGd3)
-    ctx.lineTo(xGd1, yGd1);
-    ctx.stroke();
-    ctx.closePath();
-    
-    ctx.beginPath();
-    ctx.moveTo(xGd2, yGd3);
-    ctx.lineTo(xGd1, yGd1);
-    ctx.stroke();
-    ctx.closePath();
-    
-    ctx.beginPath();
-    ctx.moveTo(xGd3, yGd3)
-    ctx.lineTo(xGd1, yGd2);
-    ctx.stroke();
-    ctx.closePath();
-    
-    ctx.beginPath();
-    ctx.moveTo(xGd2, yGd3);
-    ctx.lineTo(xGd1, yGd2);
-    ctx.stroke();
-    ctx.closePath();
-    
-    // left end of house
-    ctx.beginPath();
-    ctx.moveTo(50, 200);
-    ctx.lineTo(50, 157);
+    ctx.moveTo(30, 157);
+    ctx.lineTo(132, 144);
+    ctx.lineTo(196, 26);
+    ctx.lineTo(98, 19);
+    ctx.lineTo(30, 157);
     ctx.strokeStyle = "black";
     ctx.stroke();
     ctx.closePath();
     
-    //right end of house
+    // front wall
     ctx.beginPath();
-    ctx.moveTo(yGd2, 200);
-    ctx.lineTo(yGd2, 157);
+    ctx.moveTo(41, 147);
+    ctx.lineTo(43, 237);
+    ctx.lineTo(163, 260);
+    ctx.lineTo(164, 157);
+    ctx.lineTo(41, 147);
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+    ctx.closePath();
+    
+    // right side wall
+    ctx.beginPath();
+    ctx.moveTo(164, 157);
+    ctx.lineTo(248, 146);
+    ctx.lineTo(247, 232);
+    ctx.lineTo(163, 260);
+    ctx.stroke();
+    ctx.closePath();
+    
+    // right roof
+    ctx.beginPath();
+    ctx.moveTo(98, 19);
+    ctx.lineTo(196, 26);
+    ctx.lineTo(267, 148);
+    ctx.lineTo(165, 165);
+    ctx.lineTo(98, 19);
+    ctx.stroke();
+    ctx.closePath();
+    
+    // right chimney wall
+    ctx.beginPath();
+    ctx.moveTo(207, 90);
+    ctx.lineTo(220, 90);
+    ctx.lineTo(220, 15);
+    ctx.lineTo(207, 15);
+    ctx.lineTo(207, 90);
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+    ctx.closePath();
+    
+    //front chimney wall
+    ctx.beginPath();
+    ctx.moveTo(207, 15);
+    ctx.lineTo(187, 15);
+    ctx.lineTo(187, 60);
+    ctx.lineTo(207, 90);
     ctx.stroke();
     ctx.closePath();
 }
