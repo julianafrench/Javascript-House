94c401acc44388cce26ad65b5cbc0ac58bf70d1d
daohuukhanh <daohuukhanh@outlook.com>
2016-11-09 23:19:07 -0800

    Added windows, sun and grass.

--- house.js
+++ house.js
@@ -113,12 +113,87 @@ var drawHouse = function() {
     }
 
     function drawWindows() {
-    
-    
+        
+        //first window
+        ctx.save();
+        ctx.translate(55, 275);
+        ctx.scale(0.4,0.3);
+        ctx.beginPath();
+        ctx.moveTo(164, 465);
+        ctx.lineTo(164, 375);
+        ctx.lineTo(36, 355);
+        ctx.lineTo(36, 440);
+        ctx.lineTo(164, 460);
+        
+        ctx.moveTo(95, 450);
+        ctx.lineTo(95, 365);
+        ctx.restore();
+        
+        ctx.fillStyle = "#FFFF50"; 
+        ctx.fill();        
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = "#98510d";
+        ctx.stroke();
+        
+        //second window
+        ctx.save();
+        ctx.translate(140, 318);
+        ctx.scale(0.2,0.2);
+        ctx.beginPath();
+        ctx.moveTo(164, 370);
+        ctx.lineTo(248, 346);
+        ctx.lineTo(247, 432);
+        ctx.lineTo(163, 460);
+        ctx.closePath();
+        ctx.restore();
+        
+        ctx.fillStyle = "#FFFF50"; 
+        ctx.fill();        
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = "#98510d";
+        ctx.stroke();
+        
+        //third window
+        ctx.save();
+        ctx.translate(190, 305);
+        ctx.scale(0.2,0.2);
+        ctx.beginPath();
+        ctx.moveTo(164, 370);
+        ctx.lineTo(248, 346);
+        ctx.lineTo(247, 432);
+        ctx.lineTo(163, 460);
+        ctx.closePath();
+        ctx.restore();
+        
+        ctx.fillStyle = "#FFFF50"; 
+        ctx.fill();        
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = "#98510d";
+        ctx.stroke();
     }
     
+    function drawSun() {
+        ctx.beginPath();
+        ctx.arc(280,300,20,0,2 * Math.PI);
+        ctx.fillStyle = "#FFAF0A";
+        ctx.fill();
+    }
+    
+     function drawGrass() {
+        ctx.save();
+        ctx.scale(1,0.5);
+        ctx.beginPath();
+        ctx.arc(170,1000,220,0,2 * Math.PI);
+        ctx.restore();
+        ctx.fillStyle = "#50D050";
+        ctx.fill();
+    }
+    
+    drawSun();
+    drawGrass();
     drawWalls();
     drawRoof();
     drawChimney();
     drawDoor();
-}
+    drawWindows(); 
+};
